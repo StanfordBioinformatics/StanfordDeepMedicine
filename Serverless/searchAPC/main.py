7546c68c2293d325f57d3ecf0b27f2f20bf3fd9b
Paul Billing-Ross <pbilling@stanford.edu>
2021-04-12 19:24:14 -0700

    Added APC query & write results to blob

--- Serverless/searchAPC/main.py
+++ Serverless/searchAPC/main.py
@@ -1,6 +1,6 @@
 import base64
 from google.cloud import bigquery
-
+from google.cloud import storage
 
 def main(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
@@ -11,20 +11,37 @@ def main(event, context):
     
     print(f"This Function was triggered by messageId {context.event_id} published at {context.timestamp}.")
 
-    if 'data' in event:
-        data = base64.b64decode(event['data']).decode('utf-8')
+    #if 'data' in event:
+    data = base64.b64decode(event['data']).decode('utf-8')
     print(f"> Pubsub data: {data}.")
-    print(f"> Context: {context}.")  
+    print(f"> Context: {context}.")
 
-    #print(event['data'])
-    #client = bigquery.Client()
+    data = json.loads(data)
+
+    # Example: "projects/gbsc-gcp-class-gene222-spr21/datasets/1000g/tables/1000g_APC_PBR"
+    resource_name = data["protoPayload"]["resourceName"]
+    elements = resouce_name.split("/")
+    project_id = elements[1]
+    dataset_id = elements[3]
+    table_name = elements[5]
+
+    table_id = f"{project_id}.{dataset_id}.{table_name}" 
+
+    client = bigquery.Client()
     # Perform a query.
     QUERY = (
-      'SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` '
-      'WHERE state = "TX" '
+      'SELECT rsID ' +
+      f"FROM `{table_id}` " +
+      'WHERE start_position > 112073558 AND start_position < 112181934 ' +
       'LIMIT 100')
-#    query_job = client.query(QUERY)  # API request
-#    rows = query_job.result()  # Waits for query to finish
+    query_job = client.query(QUERY)  # API request
+    rows = query_job.result()  # Waits for query to finish
     
-#    for row in rows:
-#      print(row.name)
+    rows_string = ""
+    for row in rows:
+      rows_string += row + "\n"
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(STORAGE_BUCKET)
+    composed_blob = bucket.blob("apc-gene-rsids.txt")
+    composed_blob.upload_from_string(rows_string)
