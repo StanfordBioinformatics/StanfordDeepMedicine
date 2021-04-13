20b52ce66212b5ca55737eb6813b1449e520314d
Paul Billing-Ross <pbilling@stanford.edu>
2021-04-12 17:58:19 -0700

    Rename main method to 'main' and formatting

--- Serverless/searchAPC/main.py
+++ Serverless/searchAPC/main.py
@@ -2,21 +2,19 @@ import base64
 from google.cloud import bigquery
 
 
-def hello_pubsub(event, context):
+def main(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
     
-    print("""This Function was triggered by messageId {} published at {}
-    """.format(context.event_id, context.timestamp))
+    print(f"This Function was triggered by messageId {context.event_id} published at {context.timestamp}.")
 
     if 'data' in event:
-        name = base64.b64decode(event['data']).decode('utf-8')
-    else:
-        name = 'World'
-    print('Hello {}!'.format(name))    
+        data = base64.b64decode(event['data']).decode('utf-8')
+    print(f"> Pubsub data: {data}.")
+    print(f"> Context: {context}.")  
 
     #print(event['data'])
     #client = bigquery.Client()
